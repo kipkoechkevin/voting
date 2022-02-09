2638e9b371635025e203043920f29f63e8906f93
kevinci@live.com <123Fungua>
2022-02-10 00:12:20 +0300

    initial set up and design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Category One">Category One</option>
+                <option value="Category One">Category Two</option>
+                <option value="Category One">Category Three</option>
+                <option value="Category One">Category Four</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="category" class="w-full rounded-xl border-none px-4 py-2">
+                <option value="Filter One">Filter One</option>
+                <option value="Filter Two">Filter Two</option>
+                <option value="Filter Three">Filter Three</option>
+                <option value="Filter Four">Filter Four</option>
+            </select>
+        </div>
+        <div class="w-2/3 relative">
+            <input type="search" placeholder="Find an idea"
+                   class="w-full rounded-xl bg-white border-none placeholder-gray-900  px-4 py-2 pl-8">
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-4 text-gray-700"
+                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="m21 21-6-6m2-5a7 7 0 1 1-14 0 7 7 0 0 1 14 0z"/>
+                </svg>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
